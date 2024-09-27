b42d3b857a2e2d22c41c0aae44606bd2b4332962
MrThinkJ <ledinhthinh.ws@gmail.com>
2024-09-27 16:11:07 +0700

    Init project

---
+++ src/main/java/com/c1se22/publiclaundsmartsystem/PublicLaundSmartSystemApplication.java
@@ -0,0 +1,13 @@
+package com.c1se22.publiclaundsmartsystem;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PublicLaundSmartSystemApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PublicLaundSmartSystemApplication.class, args);
+    }
+
+}
